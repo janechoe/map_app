7881de92ee8def293c2a1253c428c40634cf09bd
Jane Choe <choejane95@gmail.com>
2017-03-14 18:32:07 -0400

    navigation with menu button works

---
+++ app/src/main/java/com/example/janechoe/myapplication/AboutActivity.java
@@ -0,0 +1,12 @@
+package com.example.janechoe.myapplication;
+
+import android.os.Bundle;
+
+public class AboutActivity extends MapsActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
--- app/src/main/java/com/example/janechoe/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/janechoe/myapplication/MapsActivity.java
@@ -1,10 +1,15 @@
 package com.example.janechoe.myapplication;
 
-import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,7 +24,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private String[] menuOptions;
     private DrawerLayout myDrawerLayout;
     private ListView myList;
-    private Fragment menuFragment;
+//    private Fragment menuFragment;
     private MapFragment mMapFragment;
 
     private GoogleMap mMap;
@@ -39,8 +44,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             fragmentTransaction.add(R.id.content_frame, mMapFragment);
             fragmentTransaction.commit();
 
-        }
+            //make menu from array indices
+            menuOptions = getResources().getStringArray(R.array.menu_array);
+            myDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+            myList= (ListView)findViewById(R.id.left_drawer);
+            //adapter for list view
+            myList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, menuOptions));
+            //list's click listener
+            myList.setOnItemClickListener(new DrawerItemClickListener());
+
+            Button menuButton = (Button) findViewById(R.id.menuButton);
+            menuButton.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    myDrawerLayout.openDrawer(Gravity.LEFT);
+                }
+            });
+            myDrawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+                @Override
+                //called when drawer is completely closed
+                public void onDrawerClosed(View view){
+                    super.onDrawerClosed(view);
+                    invalidateOptionsMenu(); //creates call to prepareoptionsmenu
+                }
+                //called when drawer is completely opened
+                public void onDrawerOpened(View view){
+                    super.onDrawerOpened(view);
+                    invalidateOptionsMenu();
+                }
+            });
+
+
+            }
+
+
     }
+
 
     /**
      * Manipulates the map once available.
@@ -51,6 +89,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -60,38 +99,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position){
+        switch(position) {
+            case 0:
+                break;
+            case 1:
+                //about
+                Intent switchIntent = new Intent(this, AboutActivity.class);
+                startActivity(switchIntent);
+                break;
+            case 2:
+                //contributors
+                //Intent switchIntent = new Intent(this, AboutActivity.class);
+                break;
+            case 3:
+                //tours
+                //Intent switchIntent = new Intent(this, AboutActivity.class);
+                break;
+            case 4:
+                //resources
+                //Intent switchIntent = new Intent(this, AboutActivity.class);
+                break;
+        }
+        //Highlight selected item
+        myList.setItemChecked(position, true);
+        //close drawer
+        myDrawerLayout.closeDrawer(myList);
+    }
+
 }
-//
-//        menuOptions = getResources().getStringArray(R.array.menu_array);
-//        myDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-//        myList= (ListView)findViewById(R.id.left_drawer);
 
-
-
-
-//        myList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, menuOptions));
-//        myList.setOnItemClickListener(new DrawerItemClickListener());
-
-
-//MAKE MAP FRAGMENT
-
-
-
-
-//
-//    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-//        @Override
-//        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//            selectItem(position);
-//        }
-//    }
-//
-//    private void selectItem(int position){
-//        Fragment fragment = new MenuFragment();
-//        Bundle args = new Bundle();
-//        //shows activity based on position
-//        args.putInt(MenuFragment.ARG_OPTION_NUMBER, position);
-//
-//
-//    }
 
